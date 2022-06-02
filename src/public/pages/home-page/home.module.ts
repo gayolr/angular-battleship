0e3f8491c0cf822061a3a2ed9a9120e9c0c19c72
Roberto <gayol@bluerival.com>
2022-06-02 05:50:00 -0500

    add configuration page and more styles

--- src/public/pages/home-page/home.module.ts
+++ src/public/pages/home-page/home.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbIconModule, NbLayoutModule } from '@nebular/theme';
+import { NbButtonModule, NbIconModule, NbLayoutModule } from '@nebular/theme';
 import { HomeComponent } from './home.component';
 import { BoardComponent } from 'src/components/board-component/board.component';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
-	imports: [CommonModule, NbIconModule, NbLayoutModule],
+	imports: [
+		CommonModule,
+		NbIconModule,
+		NbLayoutModule,
+		RouterModule,
+		NbButtonModule
+	],
 	declarations: [HomeComponent, BoardComponent]
 })
 export class HomeModule {}
--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NbLayoutModule } from '@nebular/theme';
+import { NbCardModule, NbInputModule, NbLayoutModule } from '@nebular/theme';
 import { PublicComponent } from './public.component';
 import { PublicRoutingModule } from './public.routing.module';
 import { HomeModule } from './pages/home-page/home.module';
 import { ScoreboardModule } from './pages/scoreboard-page/scoreborad.module';
-
+import { ConfigModule } from './pages/config-page/config.module';
 @NgModule({
 	imports: [
 		CommonModule,
@@ -14,8 +14,11 @@ import { ScoreboardModule } from './pages/scoreboard-page/scoreborad.module';
 		PublicRoutingModule,
 		HomeModule,
 		ScoreboardModule,
+		ConfigModule,
 		ReactiveFormsModule,
-		NbLayoutModule
+		NbLayoutModule,
+		NbInputModule,
+		NbCardModule
 	],
 	declarations: [PublicComponent],
 	exports: [],
